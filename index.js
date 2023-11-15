327e938439ef10ef898145593d583aa729010cf0
whilmarbitoco <117832470+whilmarbitoco@users.noreply.github.com>
2023-11-15 06:18:58 +0000

    Added a queue system

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 const ask = require("./ai.js");
 const fs = require("fs");
 const login = require("fca-unofficial");
-const {port} = require('./server.js')
+const  queue  = require('./queue.js')
+const { port } = require('./server.js')
 
 
 
-login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err);
+login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, api) => {
+  if (err) return console.error(err);
 
-    api.setOptions({listenEvents: true, logLevel: "silent"});
+  api.setOptions({ listenEvents: true, logLevel: "silent" });
 
-    var listenEmitter = api.listen(async(err, event) => {
-        if(err) return console.error(err);
+  var listenEmitter = api.listen(async (err, event) => {
+    if (err) return console.error(err);
 
-        switch (event.type) {
-            case "message":
-            if (event.body.startsWith("/scylla")) {
-    const prompt = event.body.substring(8);
-    const response = await ask(prompt);
-    api.sendMessage(response, event.threadID, event.messageID);
-}
-            case "event":
-                console.log(event);
-                break;
+    switch (event.type) {
+      case "message":
+        if (event.body.startsWith("/scylla")) {
+          let isQueue = queue(event.senderID)
+          if (!isQueue) {
+            const prompt = event.body.substring(8);
+            const response = await ask(prompt);
+            api.sendMessage(response, event.threadID, event.messageID);
+          } else {
+            api.sendMessage("Oppss... You're going too fast.", event.threadID, event.messageID);
+          }
         }
-    });
+        break; 
+      case "event":
+        console.log(event);
+        break;
+    }
+  });
 });
---
+++ queue.js
@@ -0,0 +1,35 @@
+let queues = [];
+
+function queue(data) {
+
+  let existDataIndex = -1;
+
+
+  for (let i = 0; i < queues.length; i++) {
+    if (queues[i].id === data) {
+      existDataIndex = i;
+      break;
+    }
+  }
+
+  if (existDataIndex !== -1) {
+  
+    const timePassed = Date.now() - queues[existDataIndex].date;
+    if (timePassed > 10000) {
+   
+      queues.splice(existDataIndex, 1);
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    queues.push({
+      id: data,
+      date: Date.now(),
+    });
+    return false;
+  }
+}
+
+module.exports = queue;
+
